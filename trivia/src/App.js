f40e5c1c0b34f104bcda280cc3ad83305d256e75
Cameron Poulton <Crp2108@hotmail.com>
2023-01-26 12:10:06 -0500

    categories display, working on questions

--- trivia/src/App.js
+++ trivia/src/App.js
@@ -1,30 +1,59 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [categories, setCategories] = useState([]);
+  return (
+    <div className="App">
+      <Category />
+    </div>
+  );
+}
 
+function Category() {
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
-    axios.get('https://opentdb.com/api_category.php')
-    .then((response) =>{ 
-      setCategories(response.data.trivia_categories);
-    });
+    axios.get("https://opentdb.com/api_category.php").then((response) => {
+      setCategories(response.data.trivia_categories)
+    })
   }, [])
 
+  const [selectedCat, setSelectedCat] = useState("")
+  if (selectedCat) {
+    console.log(selectedCat)
+    // return <CatQuestion />
+  }
 
   return (
     <section>
       <header>
         <h1>Trivia</h1>
       </header>
-      <div className="trivia-container">
-          { categories.map(category => (
-            <li> {category.id} : {category.name}</li>
+
+      <div className="category-list">
+        <ul className="category-grid">
+          {categories.map((categories) => (
+            <li
+              onClick={() => setSelectedCat(categories.name)}
+              className="category" key={categories.id}>{categories.name}
+            </li>
           ))}
+        </ul>
       </div>
     </section>
   );
-  }
+}
 
+// function CatQuestion(selectedCat) {
+//   const [question, setQuestion] = useState([])
+//   useEffect(() => {
+//     axios
+//       .get(
+//         `https://opentdb.com/api.php?amount=10`)
+//       .then((response) => {setQuestion(response.data.results.map(obj => [obj.question, obj.incorrect_answer, obj.correct_answer]))
+//       })
+//   }, [selectedCat]);
+
+
+// }
 export default App;
